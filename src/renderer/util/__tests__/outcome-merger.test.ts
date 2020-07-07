8b2ae34881c7637f423c2643813120b0278998a7
Tibo-lg <thibaut@cryptogarage.co.jp>
2020-07-07 17:37:10 +0900

    fix order in outcome merger

--- src/renderer/util/__tests__/outcome-merger.test.ts
+++ src/renderer/util/__tests__/outcome-merger.test.ts
@@ -28,6 +28,32 @@ describe('outcome merge tests', () => {
     expect(result[2].local).toBe(1)
   })
 
+  it('should merge numerical ranges with same outcomes if not ordered', () => {
+    const outcomes: Outcome[] = [
+      { message: '1', local: 1, remote: 0 },
+      { message: '3', local: 1, remote: 0 },
+      { message: '2', local: 1, remote: 0 },
+      { message: '4', local: 1, remote: 0 },
+      { message: '5', local: 0, remote: 1 },
+      { message: '6', local: 0, remote: 1 },
+      { message: '8', local: 0, remote: 1 },
+      { message: '7', local: 0, remote: 1 },
+      { message: '9', local: 1, remote: 0 },
+      { message: '10', local: 1, remote: 0 },
+      { message: '11', local: 1, remote: 0 },
+      { message: '12', local: 1, remote: 0 },
+    ]
+
+    const result = merge(outcomes)
+    expect(result.length).toBe(3)
+    expect(result[0].message).toBe('1-4')
+    expect(result[0].local).toBe(1)
+    expect(result[1].message).toBe('5-8')
+    expect(result[1].local).toBe(0)
+    expect(result[2].message).toBe('9-12')
+    expect(result[2].local).toBe(1)
+  })
+
   it('should merge numerical, but leave other values', () => {
     const outcomes: Outcome[] = [
       { message: '1', local: 1, remote: 0 },
