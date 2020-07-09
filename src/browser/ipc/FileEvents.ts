10f7d91c8e203771b569172bb01f05e7d681ebc2
Tibo-lg <thibaut@cryptogarage.co.jp>
2020-07-09 11:42:12 +0900

    Refactor IPC with base class and unregister functions

--- src/browser/ipc/FileEvents.ts
+++ src/browser/ipc/FileEvents.ts
@@ -1,24 +1,33 @@
-import { ipcMain as ipc } from 'electron-better-ipc'
-import { IPCEvents } from './IPCEvents'
-import OutcomeCall from '../../common/models/ipc/OutcomeCall'
-import OutcomeAnswer from '../../common/models/ipc/OutcomeAnswer'
 import { PARSE_OUTCOME } from '../../common/constants/IPC'
+import OutcomeAnswer from '../../common/models/ipc/OutcomeAnswer'
+import OutcomeCall from '../../common/models/ipc/OutcomeCall'
 import IOAPI from '../api/io'
+import { IPCEventsBase } from './IPCEventsBase'
+import { TaggedCallback } from './TaggedCallback'
 
-export class FileEvents implements IPCEvents {
+export class FileEvents extends IPCEventsBase {
   private _client = new IOAPI()
 
-  public registerReplies(): void {
-    ipc.answerRenderer(PARSE_OUTCOME, async data => {
-      const outcomeCall = data as OutcomeCall
-      try {
-        const outcomesList = await this._client.readOutcomes(
-          outcomeCall.outcomesPath
-        )
-        return new OutcomeAnswer(true, outcomesList)
-      } catch (e) {
-        return new OutcomeAnswer(false, [], e)
-      }
-    })
+  protected taggedCallbacks(): TaggedCallback[] {
+    return [
+      {
+        tag: PARSE_OUTCOME,
+        callback: (data): Promise<OutcomeAnswer> =>
+          this.parseOutcomeCallback(data as OutcomeCall),
+      },
+    ]
+  }
+
+  private async parseOutcomeCallback(
+    outcomeCall: OutcomeCall
+  ): Promise<OutcomeAnswer> {
+    try {
+      const outcomesList = await this._client.readOutcomes(
+        outcomeCall.outcomesPath
+      )
+      return new OutcomeAnswer(true, outcomesList)
+    } catch (e) {
+      return new OutcomeAnswer(false, [], e)
+    }
   }
 }
