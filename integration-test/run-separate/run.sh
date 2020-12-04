cfd7032c92e7e9aadfc45a71b041d0228347d48b
Tibo-lg <thibaut@cryptogarage.co.jp>
2020-12-04 16:13:12 +0900

    Improve CI and add coverage report

--- integration-test/run-separate/run.sh
+++ integration-test/run-separate/run.sh
@@ -2,9 +2,39 @@
 
 set -e
 
-for i in {1..18}
+tests=("$@")
+default=($(seq 1 1 3))
+tests=("${tests[@]:-${default[@]}}")
+
+if [ ! -z "${ISCI}" ]
+then
+  dcparam="-f docker-compose.yml -f docker-compose.ci-separate.yml -p integration-separate-${CIRCLE_NODE_INDEX}"
+  rundev="docker-compose ${dcparam} run dev"
+  dirpref="./reports/"
+  ci=":ci"
+fi
+
+mergeParam=()
+dirPaths=()
+
+coveragedir="${dirpref}coverage/integration-separate/"
+
+for i in "${tests[@]}"
 do
-  docker-compose up -d bitcoind
-  jest --config jest.config.integration-separate.js --reporters=default --runInBand -t=" $i-"
-  docker-compose down --volumes
+  directory="${coveragedir}${i}"
+  eval docker-compose ${dcparam} up -d bitcoind
+  eval ${rundev} npm run integration-separate${ci}:test -- -t="\" ${i}-\""\
+       --coverageDirectory=${directory}
+  eval docker-compose ${dcparam} down --volumes
+  mergeParam+=("${directory}/coverage-final.json")
+  dirPaths+=("${directory}")
 done
+
+eval ${rundev} npx istanbul-merge --out "${coveragedir}coverage-final${CIRCLE_NODE_INDEX}.json ${mergeParam[@]}"
+
+if [ -z "${ISCI}" ]
+then
+  npx istanbul report --include "${coveragedir}coverage-final.json --dir ${coveragedir} html"
+fi
+
+eval ${rundev} rm -r "${dirPaths[@]}"
